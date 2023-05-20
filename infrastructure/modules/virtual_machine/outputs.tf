c23257ed7dfbf76bf6b4578fa5bb001802519257
Sarah <sarahligbe12@gmail.com>
2023-05-20 01:47:11 +0100

    updated infra

--- infrastructure/modules/virtual_machine/outputs.tf
+++ infrastructure/modules/virtual_machine/outputs.tf
@@ -1,7 +1,7 @@
-#output "vm_identity_principal_id" {
-#  value       = azurerm_user_assigned_identity.vm_identity.principal_id
-#  description = "Specifies the principal id of the managed identity of the AKS cluster."
-#}
+output "vm_identity_principal_id" {
+  value       = azurerm_user_assigned_identity.vm_identity.principal_id
+  description = "Specifies the principal id of the managed identity of the AKS cluster."
+}
 
 output "public_ip" {
   value = azurerm_linux_virtual_machine.main.public_ip_address
