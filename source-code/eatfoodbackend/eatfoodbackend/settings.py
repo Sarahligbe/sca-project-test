5168d8e638212506ae004876103e1f5dae7c796b
Sarahligbe <sarahligbe12@gmail.com>
2023-05-20 13:45:56 +0000

    new

--- source-code/eatfoodbackend/eatfoodbackend/settings.py
+++ source-code/eatfoodbackend/eatfoodbackend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qc$)%2oo61y4@@ni$tqitg9i7y(_onup*2p_^w95#bbi*k6ey_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', '46.101.19.75']
 
 
 # Application definition
@@ -82,11 +82,11 @@ WSGI_APPLICATION = 'eatfoodbackend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config("POSTGRES_DB", 'postgres'),
-        'USER': config('POSTGRES_USER','postgres'),
-        'PASSWORD': config('POSTGRES_PASSWORD','postgres'),
-        'HOST': config('POSTGRES_HOST','db'),
-        'PORT': config('POSTGRES_PORT','5432'),
+        'NAME': os.environ['POSTGRES_DB'],
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+        'HOST': os.environ['POSTGRES_HOST'],
+        'PORT': int(os.environ['POSTGRES_PORT']),
     }
 }
 
